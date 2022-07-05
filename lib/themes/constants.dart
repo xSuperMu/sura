4ab1c29246a1b055401a748a90e3d6ebb2752219
Sama Almehmadi <samanaroto3@gmail.com>
2022-07-06 02:05:19 +0300

    add grey color

--- lib/themes/constants.dart
+++ lib/themes/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 //colors used in this app
-const Color whiteBlue = Color.fromRGBO(117, 189, 224, 1);
+ const  Color whiteBlue = Color.fromRGBO(117, 189, 224, 1);
 const Color babyBlue = Color.fromRGBO(162, 226, 248, 1);
 const Color darkBlue = Color.fromRGBO(74, 141, 183, 1);
 const Color white = const Color.fromRGBO(255, 255, 255, 1);
 const Color black = const Color.fromRGBO(0, 0, 0, 1);
-
+const Color grey =  Color.fromARGB(20, 5, 0, 5);
 //font
 const String montserrat = 'Montserrat';
