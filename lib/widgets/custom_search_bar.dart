2bf7863240d92f7204771d83894063c666d13cf5
Sama Almehmadi <samanaroto3@gmail.com>
2022-07-23 16:19:25 +0300

    edit custom_search_bar adding searchEnabled and onTextChane

--- lib/widgets/custom_search_bar.dart
+++ lib/widgets/custom_search_bar.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -5,50 +6,52 @@ import 'package:flutter/src/foundation/key.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+class SearchBar extends StatefulWidget {
+  final bool searchEnabled;
+  final Function(String)? onTextChange;
 
-class SearchBar extends StatefulWidget  {
+  SearchBar({this.searchEnabled = false, this.onTextChange});
+
   @override
   State<SearchBar> createState() => _SearchBarState();
 }
 
 class _SearchBarState extends State<SearchBar> {
-   GoogleMapController? mapController; 
+  GoogleMapController? mapController;
 
-  String location = "أبحث"; 
-  
-  
+  String location = "أبحث";
 
-      @override
-      Widget build(BuildContext context) {
-        return Positioned(
-        //search input bar
-        top: 10,
-        child: Padding(
-          padding: EdgeInsets.all(15),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0), 
-            ),
-                       child: Container(
-                         padding: EdgeInsets.all(0),
-                         width: MediaQuery.of(context).size.width - 40,
-                         child: ListTile(
-                            title:Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-
-                                Text(location, style: TextStyle(fontSize: 18),),
-                              ],
-                            ),
-                            trailing: Icon(Icons.search),
-                            dense: true,
-                         )
-                       ),
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(0),
+          width: max(MediaQuery.of(context).size.width - 40, 0),
+          child: ListTile(
+            title: widget.searchEnabled
+                ? TextField(
+                    onChanged: widget.onTextChange,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.zero,
                     ),
-                 )
-               );
-             
-    }
-
-    
+                  )
+                : Text(
+                    location,
+                    style: TextStyle(fontSize: 18),
+                    textDirection: TextDirection.rtl,
+                  ),
+            leading: Icon(Icons.search),
+            dense: true,
+          ),
+        ),
+      ),
+    );
+  }
 }
