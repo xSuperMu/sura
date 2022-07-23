981e2d7b31fc041f2c3898d61c38de51334af343
Sama Almehmadi <samanaroto3@gmail.com>
2022-07-23 16:01:08 +0300

    declare map list locations and searchResulst , location for set value of loaction and removeLocation for remove location

--- lib/models/providers/control_provider.dart
+++ lib/models/providers/control_provider.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/foundation.dart';
 
-class ControlProvuder with ChangeNotifier {
-  //data
+class DataProvider with ChangeNotifier {
+  List<Map<String, dynamic>> _locations = [];
+  List<Map<String, dynamic>> _searchResults = [];
+
+  set locations(List<Map<String, dynamic>> value) {
+    _locations = value;
+    notifyListeners();
+  }
+
+  List<Map<String, dynamic>> get locations {
+    return _locations;
+  }
+
+  
+  //when user clic delete button will be deleted 
+  void removeLocation(Map<String, dynamic> location) {
+      _locations.removeWhere((element) => element['placeId'] == location['placeId']);
+      notifyListeners();
+  }
 }
