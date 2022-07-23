760a8ffa85d74b106a3c02704d07f7e6c95d05aa
Sama Almehmadi <samanaroto3@gmail.com>
2022-07-23 15:16:03 +0300

    add multiProvider and new thems for text and add LocaliztionsDelegates for Arabic language

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sura/models/providers/control_provider.dart';
+
 import 'package:sura/models/screens/home.dart';
 import 'package:sura/themes/constants.dart';
 import 'models/screens/Onboarding.dart';
@@ -13,22 +17,44 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   final bool showHome;
+
   const MyApp({Key? key, required this.showHome}) : super(key: key);
 
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+
+    return MultiProvider(
+      
+      providers: [
+        ChangeNotifierProvider(create: (context) => DataProvider()),
+      ],
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
+
         theme: ThemeData(
           textTheme: const TextTheme(
             bodyText1: TextStyle(fontSize: 20.0),
             bodyText2: TextStyle(fontSize: 18.0),
             button: TextStyle(fontSize: 18.0),
           ),
+
+
           fontFamily: montserrat,
         ),
-        home: showHome ? Home() : Onboarding());
+         
+        home: showHome ? Home() : Onboarding(),
+        //this is for lnaguage and right to left 
+        localizationsDelegates: [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale("ar", "AE"), 
+        ],
+      ),
+    );
   }
 }
