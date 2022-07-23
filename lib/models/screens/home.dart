5e6a45c8204d5fbc0bce2960d2d0aabd306245c1
Sama Almehmadi <samanaroto3@gmail.com>
2022-07-23 16:38:45 +0300

    add searchBar and SearchScreen

--- lib/models/screens/home.dart
+++ lib/models/screens/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places_sdk/flutter_google_places_sdk.dart'
+    as placesSdk;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sura/models/screens/search.dart';
+
 
 import '../../themes/constants.dart';
 import '../../widgets/custom_bottom_sheet.dart';
@@ -15,69 +19,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-   //for test 
-    static const _map = CameraPosition(
+  //for test
+  static const _map = CameraPosition(
     target: LatLng(24.6500, 46.7100),
     zoom: 11.5,
   );
-  GoogleMapController? mapController; 
-  String location = "Search Location"; 
+  GoogleMapController? mapController;
+  String location = "Search Location";
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-       //  Container(child: BottomSheetCoustom()),
-       // extendBodyBehindAppBar: true,
-    
         body: Stack(
-          
-           children: [
-             LayoutBuilder(
-              builder: (BuildContext context , BoxConstraints constraints ) {
-                  
-                  return  Container(
-                    decoration: BoxDecoration(
-                      color: white,
-                       borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                       topRight: Radius.circular(40))),
-                       child: SizedBox(
-                           
-                         height: constraints.maxHeight/2,
-                         child: GoogleMap(
-                             initialCameraPosition: _map,
-                             mapType: MapType.normal,
-                             
-                              onMapCreated: (controller) { //method called when map is created
-                            setState(() {
-                           mapController = controller; 
-                            });
-                              },
-                             ),
-                             
-                    ),
-                  );
-                  
-              }),
-              
-            SearchBar(),
-            
-            BottomSheetCoustom(),
-        ],  
-      
-        
-        
+          children: [
+            LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: SizedBox(
+                  height: constraints.maxHeight / 2,
+                  child: GoogleMap(
+                    initialCameraPosition: _map,
+                    mapType: MapType.normal,
+                    onMapCreated: (controller) {
+                      //method called when map is created
+                      setState(() {
+                        mapController = controller;
+                      });
+                    },
+                  ),
+                ),
+              );
+            }),
+            InkWell(
+              child: SearchBar(),
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SearchScreen(),
+                ),
+              ),
+            ),
+            BottomSheetCustom(),
+          ],
         ),
-          
-          
-
-
-
-        ),
-       
-    
-      
+      ),
     );
   }
 }
